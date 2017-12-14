79fd4ac4a47c39143c97c524972116c6a7cc8919
Jason Horner <BigJimslade@gmail.com>
2017-12-14 13:41:05 +0100

    adding objects

---
+++ ETL-DesignPatterns/Databases/Staging/AWLTSRC/Stored Procedures/CustomerPreLoad_HASH.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[CustomerPreLoad_HASH]
+	@initStagingTable bit = 0
+AS
+BEGIN
+
+SET NOCOUNT ON;
+
+IF @initStagingTable  = 1
+BEGIN
+
+TRUNCATE TABLE [AWLTSRC].[Customer];
+
+END
+
+
+-- Remove Data From Staging tables
+TRUNCATE TABLE [AWLTSRC].[Customer_DEL];
+TRUNCATE TABLE [AWLTSRC].[Customer_UPD];
+TRUNCATE TABLE [AWLTSRC].[Customer_INS];
+	
+
+RETURN 0
+END
