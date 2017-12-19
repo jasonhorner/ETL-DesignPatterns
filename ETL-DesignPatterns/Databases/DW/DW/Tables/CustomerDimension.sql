97f1b7dd14ec393848b37ac14c9e91c7f6b316bc
Jason Horner <BigJimslade@gmail.com>
2017-12-19 09:55:15 +0100

    updating DW project

---
+++ ETL-DesignPatterns/Databases/DW/DW/Tables/CustomerDimension.sql
@@ -0,0 +1,3 @@
+﻿CREATE TABLE [DW].[CustomerDimension](
+	[CustomerSK] [int] IDENTITY(1,1) NOT NULL
+) ON [PRIMARY]
---
+++ ETL-DesignPatterns/Databases/DW/DW/Tables/ProductDimension.sql
@@ -0,0 +1,3 @@
+﻿CREATE TABLE [DW].[ProductDimension](
+	[ProductDimensionSK] [int] IDENTITY(1,1) NOT NULL
+) ON [PRIMARY]
