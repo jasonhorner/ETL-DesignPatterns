66f590032f5258791e84b59656f52667087abede
Jason Horner <BigJimslade@gmail.com>
2018-01-05 15:03:03 -0700

    Updated CDC and CT Code

---
+++ ETL-DesignPatterns/Databases/Staging/AWLTSRC/Stored Procedures/SalesLT_Customer_Delete.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [AWLTSRC].[SalesLT_Customer_Delete]
+
+AS
+	BEGIN
+		SET NOCOUNT ON;
+		--TODO Add Error Handling, Logging, and Row Counts
+
+		-- Hard Delete
+		DELETE FROM  dest
+		FROM AWLTSRC.Customer AS dest
+		WHERE EXISTS (
+		SELECT * FROM AWLTSRC.Customer_DEL del WHERE dest.CustomerId = del.CustomerId
+		);
+
+		-- Soft Delete
+		UPDATE dest
+		SET 
+		-- isDeleted = 1
+		--,UpdatedDate = GetDate()
+		dest.CustomerID = dest.CustomerID
+		FROM [AWLTSRC].[SalesLT_Customer] AS dest
+		WHERE EXISTS (
+		SELECT * FROM AWLTSRC.Customer_DEL src WHERE dest.CustomerId = src.CustomerId
+		);
+		
+
+
+RETURN 0
+END
--- ETL-DesignPatterns/Databases/Staging/Security/AWDWSRC.sql
+++ ETL-DesignPatterns/Databases/Staging/Security/AWDWSRC.sql
@@ -1,3 +1,5 @@
 ﻿CREATE SCHEMA [AWDWSRC]
     AUTHORIZATION [dbo];
 
+
+
