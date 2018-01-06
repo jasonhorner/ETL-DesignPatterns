811eb48af01f0fa901429ef29c679f515233a394
Jason Horner <BigJimslade@gmail.com>
2018-01-06 16:10:17 -0700

    adding disable scripts

--- ETL-DesignPatterns/Databases/Staging/Scripts/Enable Change Data Capture.sql
+++ ETL-DesignPatterns/Databases/Staging/Scripts/Enable Change Data Capture.sql
@@ -12,21 +12,19 @@ IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
 GO
 
 USE [$(DatabaseName)];
-
-
 GO
 
 -- Enable CDC at the database level
 -- https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sys-sp-cdc-enable-table-transact-sql
 
-exec sys.sp_cdc_enable_db;
+--exec sys.sp_cdc_enable_db;
 
 
-/*
+
 exec sys.sp_cdc_enable_table
  	'$(SchemaName)', '$(TableName)', '[$(SchemaName)].[$(TableName)] ETL CDC Tracking',
  	@supports_net_changes = 1, @role_name = null;
-*/
+
 -- Verification
 
 SELECT [name], is_cdc_enabled, * from sys.databases
@@ -45,3 +43,25 @@ WHERE tb.is_tracked_by_cdc = 1
 declare @start_time datetime, @capturestarted bit;
 
 select CASE WHEN min(tran_end_time) IS NULL THEN 'CDC Not Started' ELSE 'CDC Started' END AS CDCStatus from cdc.lsn_time_mapping;
+
+USE AdventureWorks2012;  
+GO  
+EXEC sys.sp_cdc_start_job;  
+GO  
+
+-- Disable CDC at table Level
+USE [$(DatabaseName)]
+GO
+
+EXEC sys.sp_cdc_disable_table  
+@source_schema = N'$(SchemaName)',  
+@source_name   = N'$(TableName)',  
+@capture_instance = N'[$(SchemaName)].[$(TableName)] ETL CDC Tracking'  
+GO  
+
+-- disable CDC at DB level
+USE [$(DatabaseName)];
+GO  
+EXEC sys.sp_cdc_disable_db  
+GO  
+
